20976826ff90645d9c2f2a20bfd3685e4336b5f8
thevalentinedev <hello@valentine.dev>
2025-06-08 16:29:28 -0400

    refactor(cron): remove cron-related components and endpoints

--- app/admin/cron/page.tsx
+++
@@ -1,9 +0,0 @@
-import CronDashboard from "@/components/admin/cron-dashboard"
-
-export default function CronAdminPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <CronDashboard />
-    </div>
-  )
-}
